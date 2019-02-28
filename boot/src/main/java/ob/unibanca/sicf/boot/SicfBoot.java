512e40bfedaa4ce68483b439a5d25ed2a33398eb
hanzllcc <hllantocc@odybank.com.pe>
2019-02-28 14:38:33 -0500

    chore: Configuración del filtro CORS

--- boot/src/main/java/ob/unibanca/sicf/boot/SicfBoot.java
+++ boot/src/main/java/ob/unibanca/sicf/boot/SicfBoot.java
@@ -4,6 +4,13 @@ import ob.unibanca.sicf.facturacion.FacturacionBoot;
 import ob.unibanca.sicf.mantenimientosgenerales.MantenimientosGeneralesBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Esta clase se encarga de arrancar los módulos declarados en el pom.xml
@@ -17,5 +24,23 @@ public class SicfBoot {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SicfBoot.class, args);
+	}
+	
+	/**
+	 * En este método se configuran la integración con distintos origenes de petición.
+	 * <p>
+	 * <b>Nota:</b> Esta método será reubicada
+	 * </p>
+	 *
+	 * @return CorsFilert con los origenes configurados
+	 */
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration(); config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*")); config.setAllowedHeaders(
+				Arrays.asList("Origin", "Content-Type", "Accept")); config.setAllowedMethods(
+				Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")); source.registerCorsConfiguration(
+				"/**", config); return new CorsFilter(source);
 	}
 }
