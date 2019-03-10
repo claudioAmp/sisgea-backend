3754ce4b0da7cdb8656b54d3d55c8afd317d28ac
hanzllcc <hllantocc@odybank.com.pe>
2019-03-10 14:52:04 -0500

    chore: Eliminación de Groups en validación de la clase ParametroSistema

--- mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/model/ParametroSistema.java
+++ mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/model/ParametroSistema.java
@@ -1,16 +1,14 @@
 package ob.unibanca.sicf.mantenimientosgenerales.model;
 
-import java.util.Date;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ob.commons.validation.validation.CaracterRequerido;
 import ob.commons.validation.validation.IdNumerico;
-import ob.commons.validation.validation.group.IRegistro;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
 @Builder
@@ -20,17 +18,16 @@ public class ParametroSistema {
 	
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	private Date fechaProceso;
-	@IdNumerico(maxRange = 99999, groups = IRegistro.class)
+	@IdNumerico(maxRange = 99999)
 	private int idInstitucion;
 	private String descripcionInstitucion;
-	@IdNumerico(maxRange = 999, groups = IRegistro.class)
+	@IdNumerico(maxRange = 999)
 	private int idMonedaProcesador;
 	private String descripcionMonedaProcesador;
 	private double porcentajeIGV;
 	private double porcentajeRenta;
 	private double porcentajeDetraccion;
 	private double limiteDetraccion;
-	@CaracterRequerido(maxLength = 20, regexpPattern="[0-9]+")
+	@CaracterRequerido(maxLength = 20, regexpPattern = "[0-9]+")
 	private String cuentaDetraccion;
-	
 }
