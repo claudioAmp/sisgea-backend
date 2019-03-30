4a10f6b3086a08eb73e9c4af079394e17a43cea5
hanzllcc <hanzllcc@gmail.com>
2019-03-30 11:54:54 -0500

    style: Adición de linea en blanco

--- facturacion/src/main/java/ob/unibanca/sicf/facturacion/mapper/ICodigoFacturacionMapper.java
+++ facturacion/src/main/java/ob/unibanca/sicf/facturacion/mapper/ICodigoFacturacionMapper.java
@@ -3,6 +3,7 @@ package ob.unibanca.sicf.facturacion.mapper;
 import ob.commons.mantenimiento.mapper.IMantenibleMapper;
 import ob.unibanca.sicf.facturacion.model.CodigoFacturacion;
 import org.apache.ibatis.annotations.Mapper;
+
 @Mapper
 public interface ICodigoFacturacionMapper extends IMantenibleMapper<CodigoFacturacion> {
 
