34413a735a633a03685f646447891509bd4e3162
carlaulloa <carla.ulloa.lp@gmail.com>
2019-04-10 19:05:49 -0500

    feat: update TipoCuentaCompensacionService

--- mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/service/tipocuentacompensacion/TipoCuentaCompensacionService.java
+++ mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/service/tipocuentacompensacion/TipoCuentaCompensacionService.java
@@ -9,17 +9,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import ob.commons.mantenimiento.mapper.IMantenibleMapper;
 import ob.commons.mantenimiento.service.MantenibleService;
-import ob.unibanca.sicf.mantenimientosgenerales.mapper.ITipoCuentaCompensacionMapper;
 import ob.unibanca.sicf.mantenimientosgenerales.model.TipoCuentaCompensacion;
 
 @Service
 public class TipoCuentaCompensacionService extends MantenibleService<TipoCuentaCompensacion> implements ITipoCuentaCompensacionService{
 
-	private final ITipoCuentaCompensacionMapper tipoCuentaCompensacionMapper;
-	
 	public TipoCuentaCompensacionService(@Qualifier("ITipoCuentaCompensacionMapper") IMantenibleMapper<TipoCuentaCompensacion> mantenibleMapper) {
 		super(mantenibleMapper);
-		this.tipoCuentaCompensacionMapper = (ITipoCuentaCompensacionMapper) mantenibleMapper;
 	}
 	
 	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
