f81f914fd9bc81a2d974fb3a3a6ecb1e1e04366f
carlaulloa <carla.ulloa.lp@gmail.com>
2019-04-14 01:41:10 -0500

    feat: avance consulta swdmplog

--- consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/service/swdmplog/ISwdmplogService.java
+++ consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/service/swdmplog/ISwdmplogService.java
@@ -1,5 +1,11 @@
 package ob.unibanca.sicf.consultasgenerales.service.swdmplog;
 
+import java.util.List;
+
+import ob.unibanca.sicf.consultasgenerales.model.criterio.swdmplog.CriterioBusquedaSwdmplog;
+import ob.unibanca.sicf.consultasgenerales.model.swdmplog.TxnSwdmplog;
+
 public interface ISwdmplogService {
 
+	List<TxnSwdmplog> buscarPorCriterios(CriterioBusquedaSwdmplog criterioBusqueda);
 }
--- consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/service/swdmplog/SwdmplogService.java
+++ consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/service/swdmplog/SwdmplogService.java
@@ -1,8 +1,14 @@
 package ob.unibanca.sicf.consultasgenerales.service.swdmplog;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+
 import ob.unibanca.sicf.consultasgenerales.mapper.ISwdmplogMapper;
+import ob.unibanca.sicf.consultasgenerales.model.criterio.swdmplog.CriterioBusquedaSwdmplog;
+import ob.unibanca.sicf.consultasgenerales.model.swdmplog.TxnSwdmplog;
 
 @Service
 public class SwdmplogService implements ISwdmplogService {
@@ -11,5 +17,11 @@ public class SwdmplogService implements ISwdmplogService {
 	
 	public SwdmplogService(ISwdmplogMapper swdmplogMapper) {
 		this.swdmplogMapper = swdmplogMapper;
+	}
+
+	@Override
+	public List<TxnSwdmplog> buscarPorCriterios(CriterioBusquedaSwdmplog criterioBusqueda) {
+		PageHelper.startPage(criterioBusqueda.getPageNum(), criterioBusqueda.getPageSize());
+		return this.swdmplogMapper.buscarPorCriterios(criterioBusqueda);
 	}
 }
