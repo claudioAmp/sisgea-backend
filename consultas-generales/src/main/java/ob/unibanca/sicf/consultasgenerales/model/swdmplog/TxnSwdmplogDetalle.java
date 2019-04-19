fe98dc7cac749df9ee2f4e0618207271512e03b7
CrisDC <cristhianw11@hotmail.com>
2019-04-19 13:26:06 -0500

    feat: actualización modelos swdmplog - swdmplogdetalle

--- consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/model/swdmplog/TxnSwdmplogDetalle.java
+++ consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/model/swdmplog/TxnSwdmplogDetalle.java
@@ -18,7 +18,7 @@ public class TxnSwdmplogDetalle {
 	private String stfwdIndicator;
 	private String pan;
 	private String processingCode;
-	private double transactionAmount;
+	private String descripcionCodProcSw;
 	private Date transmissionDate;
 	private String transmissionTime;
 	private String traceNumber;
@@ -27,18 +27,22 @@ public class TxnSwdmplogDetalle {
 	private String expiryDate;
 	private Date captureDate;
 	private String merchantType;
+	private String descripcionGiroNegocio;
 	private String posEntryMode;
 	private String posConditionCode;
 	private double settAmountTxnFee;
 	private String acquiringInstitution;
+	private String descripcionBINInstAdq;
 	private String forwardingInstitution;
+	private String descripcionBINIntReenv;
 	private String referenceNumber;
 	private String authorizationCode;
 	private int responseCode;
-	private String descripcionResponseCode;
+	private String descripcionCodigoRptaSw;
 	private String cardAcceptorTermId;
 	private String cardAcceptorLocation;
 	private int transactionCurrency;
+	private String descripcionMonedaTransaccion;
 	private String origMessageType;
 	private String origTraceNumber;
 	private Date origDate;
@@ -46,9 +50,35 @@ public class TxnSwdmplogDetalle {
 	private String origAcquiringInst;
 	private String origForwardingInst;
 	private String requestingInstitution;
+	private String descripcionBINInstEmi;
 	private String accountIdentification1;
 	private String accountIdentification2;
 	private String cardcategory;
 	private Date fechaProceso;
+	
+	private String idMembresia;
+	private String descripcionMembresia;
+	private String idServicio;
+	private String descripcionServicio;
+	private int idOrigen;
+	private String descripcionOrigen;
+	private int idClaseTransaccion;
+	private String descripcionClaseTransaccion;
+	private int idCodigoTransaccion;
+	private String descripcionCodigoTransaccion;
+	private int idCanal;
+	private String descripcionCanal;
+	private int idInstitucionEmisora;
+	private String descripcionInstEmisora;
+	private int idInstitucionReceptora;
+	private String descripcionIntReceptora;
+	private int idATM;
+	private String nombreATM;
+	private String idProceso;
+	private String descripcionProceso;
+	private String idBIN;
+	private String descripcionBIN;
+	private int idRol;
+	private String descripcionRolTransaccion;
 
 }
