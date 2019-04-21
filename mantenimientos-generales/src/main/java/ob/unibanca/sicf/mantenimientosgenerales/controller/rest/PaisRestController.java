7f28e9c1c9056656707c8f77e2d801066bec26ac
Karen MZ <karen.munoz@unmsm.edu.pe>
2019-04-21 11:10:17 -0500

    feat:Rescontroller,service, mapper, model Pais

---
+++ mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/controller/rest/PaisRestController.java
@@ -0,0 +1,21 @@
+package ob.unibanca.sicf.mantenimientosgenerales.controller.rest;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import ob.unibanca.sicf.mantenimientosgenerales.model.Pais;
+import ob.unibanca.sicf.mantenimientosgenerales.service.pais.IPaisService;
+@Controller
+public class PaisRestController {
+private final IPaisService paisService;
+	
+	public PaisRestController(IPaisService paisService) {
+		this.paisService = paisService;
+	}
+	@GetMapping("/paises")
+	public List<Pais> buscarTodosPaises() {
+		return this.paisService.buscarTodosPaises();
+	}
+	
+}
