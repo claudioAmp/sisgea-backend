afcc797f1175595efa581895ea5d20a8f8443165
jackore <39741286+jackore@users.noreply.github.com>
2019-04-21 09:49:31 -0500

    feat: Adición Reporte MAE_MODO_ENTRADA_POS

---
+++ mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/controller/exportacion/ModoEntradaPOSExportacionController.java
@@ -0,0 +1,37 @@
+package ob.unibanca.sicf.mantenimientosgenerales.controller.exportacion;
+
+import ob.unibanca.sicf.mantenimientosgenerales.model.ModoEntradaPOS;
+import ob.unibanca.sicf.mantenimientosgenerales.service.modoentradapos.IModoEntradaPOSService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.Map;
+import java.util.HashMap;
+
+import java.util.List;
+
+@Validated
+@Controller
+public class ModoEntradaPOSExportacionController {
+	
+	private final IModoEntradaPOSService modoEntradaPOSService;
+	
+	public ModoEntradaPOSExportacionController(IModoEntradaPOSService modoEntradaPOSService) {
+		this.modoEntradaPOSService = modoEntradaPOSService;
+	}
+	
+	@GetMapping(value = "/modos-entradas-pos.xlsx")
+	public ModelAndView exportarModoEntradaPOS(ModelMap model) {
+		Map<String, Object> params = new HashMap<>();
+		List<ModoEntradaPOS> lista = modoEntradaPOSService.buscarTodosModosEntradasPOS();
+      params.put("mantenimiento", lista);
+      model.addAttribute("template", "mantenimientosgenerales/modoEntradaPOS");
+      model.addAttribute("name", "Reporte ModoEntradaPOS");
+      model.addAttribute("params", params);
+      return new ModelAndView("jxlsView", model);
+	}
+
+}
