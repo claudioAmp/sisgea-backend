22ad4b9c6602cbcc7ed98e962e5fb0ee859c8ad2
jackore <39741286+jackore@users.noreply.github.com>
2019-04-21 09:52:08 -0500

    Feat: Adición Reporte MAE_TIPO_TARIFA

---
+++ tarifarios/src/main/java/ob/unibanca/sicf/tarifarios/controller/exportacion/TipoTarifaExportacionController.java
@@ -0,0 +1,37 @@
+package ob.unibanca.sicf.tarifarios.controller.exportacion;
+
+import ob.unibanca.sicf.tarifarios.model.TipoTarifa;
+import ob.unibanca.sicf.tarifarios.service.tipotarifa.ITipoTarifaService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.Map;
+import java.util.HashMap;
+
+import java.util.List;
+
+@Validated
+@Controller
+public class TipoTarifaExportacionController {
+	
+	private final ITipoTarifaService tipoTarifaService;
+	
+	public TipoTarifaExportacionController(ITipoTarifaService tipoTarifaService) {
+		this.tipoTarifaService = tipoTarifaService;
+	}
+	
+	@GetMapping(value = "/tipos-tarifas.xlsx")
+	public ModelAndView exportarTipoTarifa(ModelMap model) {
+		Map<String, Object> params = new HashMap<>();
+		List<TipoTarifa> lista = tipoTarifaService.buscarTodosTiposTarifas();
+      params.put("mantenimiento", lista);
+      model.addAttribute("template", "tarifarios/tipoTarifa");
+      model.addAttribute("name", "Reporte TipoTarifa");
+      model.addAttribute("params", params);
+      return new ModelAndView("jxlsView", model);
+	}
+
+}
