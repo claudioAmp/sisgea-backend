09d2b342f42b51000d717924cb10210253d1326e
mariomace20 <mario.cortez1@unmsm.edu.pe>
2019-04-23 05:10:37 -0500

    fix: tabla mant reporteador

--- reportes/src/main/java/ob/unibanca/sicf/reportes/mapper/ITablaMapper.java
+++ reportes/src/main/java/ob/unibanca/sicf/reportes/mapper/ITablaMapper.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import ob.commons.mantenimiento.mapper.IMantenibleMapper;
 import ob.unibanca.sicf.reportes.model.Tabla;
 
 @Mapper
-public interface ITablaMapper {
+public interface ITablaMapper extends IMantenibleMapper<Tabla>{
 	List<Tabla> buscarTodos();
 }
