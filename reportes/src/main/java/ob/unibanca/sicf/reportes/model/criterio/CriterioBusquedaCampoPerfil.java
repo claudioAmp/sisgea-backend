45a8c5a1b68c7f22e210dfd5f2b777953acee24f
vieriGarcia <45087989+vieriGarcia@users.noreply.github.com>
2019-04-25 17:49:01 -0500

    feat: campoPerfil

---
+++ reportes/src/main/java/ob/unibanca/sicf/reportes/model/criterio/CriterioBusquedaCampoPerfil.java
@@ -0,0 +1,16 @@
+package ob.unibanca.sicf.reportes.model.criterio;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CriterioBusquedaCampoPerfil{
+	String descripcionPerfil;
+}
