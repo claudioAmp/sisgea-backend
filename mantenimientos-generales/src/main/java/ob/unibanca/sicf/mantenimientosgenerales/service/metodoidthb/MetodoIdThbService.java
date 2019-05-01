03b29a469a06137d0fefee02dd5f891ae21a0f9d
jackore <39741286+jackore@users.noreply.github.com>
2019-05-01 02:59:21 -0500

    feat: Eliminación de comentarios, corrección id

--- mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/service/metodoidthb/MetodoIdThbService.java
+++ mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/service/metodoidthb/MetodoIdThbService.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ob.unibanca.sicf.mantenimientosgenerales.service.metodoidthb;
 
 import java.util.List;
@@ -16,10 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- *
- * @author Asus
- */
 @Service
 public class MetodoIdThbService extends MantenibleService<MetodoIdThb> implements IMetodoIdThbService{
     
