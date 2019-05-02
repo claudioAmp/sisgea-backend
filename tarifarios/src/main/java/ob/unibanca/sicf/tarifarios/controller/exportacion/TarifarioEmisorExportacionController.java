b63f4879564dde50def7baab095d9c9f4f056d5f
jackore <39741286+jackore@users.noreply.github.com>
2019-05-02 10:41:00 -0500

    feat: Adición Exportación TarifarioEmisor

---
+++ tarifarios/src/main/java/ob/unibanca/sicf/tarifarios/controller/exportacion/TarifarioEmisorExportacionController.java
@@ -0,0 +1,37 @@
+package ob.unibanca.sicf.tarifarios.controller.exportacion;
+
+import ob.unibanca.sicf.tarifarios.model.TarifarioEmisor;
+import ob.unibanca.sicf.tarifarios.service.tarifarioemisor.ITarifarioEmisorService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.Map;
+import java.util.HashMap;
+
+import java.util.List;
+
+@Validated
+@Controller
+public class TarifarioEmisorExportacionController {
+	
+	private final ITarifarioEmisorService tarifarioEmisorService;
+	
+	public TarifarioEmisorExportacionController(ITarifarioEmisorService tarifarioEmisorService) {
+		this.tarifarioEmisorService = tarifarioEmisorService;
+	}
+	
+	@GetMapping(value = "/tarifarios-emisores.xlsx")
+	public ModelAndView exportarTarifarioEmisor(ModelMap model) {
+		Map<String, Object> params = new HashMap<>();
+		List<TarifarioEmisor> lista = tarifarioEmisorService.buscarTodosTarifariosEmisores();
+      params.put("mantenimiento", lista);
+      model.addAttribute("template", "tarifarios/tarifarioEmisor");
+      model.addAttribute("name", "Reporte Tarifario Emisor");
+      model.addAttribute("params", params);
+      return new ModelAndView("jxlsView", model);
+	}
+
+}
