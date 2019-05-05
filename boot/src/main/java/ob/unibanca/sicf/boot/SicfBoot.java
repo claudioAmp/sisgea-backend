5cc5a36a354d428cd6c60424aca76fd12e5b19ed
hanzllcc <hanzllcc@gmail.com>
2019-05-05 17:13:06 -0500

    feat: Adición propiedad ob.commons-autorizacion-jwt.canal-seguro y modificación en logback-spring.xml

--- boot/src/main/java/ob/unibanca/sicf/boot/SicfBoot.java
+++ boot/src/main/java/ob/unibanca/sicf/boot/SicfBoot.java
@@ -1,8 +1,12 @@
 package ob.unibanca.sicf.boot;
 
-import java.util.Arrays;
-import java.util.Collections;
-
+import ob.commons.commonsjwt.JWTUtil;
+import ob.unibanca.sicf.consultasgenerales.ConsultasGeneralesBoot;
+import ob.unibanca.sicf.facturacion.FacturacionBoot;
+import ob.unibanca.sicf.generadorconsultas.GeneradorConsultasBoot;
+import ob.unibanca.sicf.mantenimientosgenerales.MantenimientosGeneralesBoot;
+import ob.unibanca.sicf.reportes.ReportesBoot;
+import ob.unibanca.sicf.tarifarios.TarifariosBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,13 +19,9 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.ResourceBundleViewResolver;
 
-import ob.unibanca.sicf.consultasgenerales.ConsultasGeneralesBoot;
-import ob.unibanca.sicf.facturacion.FacturacionBoot;
-import ob.unibanca.sicf.generadorconsultas.GeneradorConsultasBoot;
-import ob.unibanca.sicf.mantenimientosgenerales.MantenimientosGeneralesBoot;
-import ob.unibanca.sicf.reportes.ReportesBoot;
-import ob.unibanca.sicf.tarifarios.TarifariosBoot;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -34,8 +34,9 @@ import java.util.List;
  *
  * @author Hanz Llanto
  */
-@SpringBootApplication(scanBasePackageClasses = {MantenimientosGeneralesBoot.class, FacturacionBoot.class, 
-		TarifariosBoot.class,ConsultasGeneralesBoot.class, ReportesBoot.class,GeneradorConsultasBoot.class})
+@SpringBootApplication(
+		scanBasePackageClasses = {MantenimientosGeneralesBoot.class, FacturacionBoot.class, TarifariosBoot.class,
+		                          ConsultasGeneralesBoot.class, ReportesBoot.class, GeneradorConsultasBoot.class})
 public class SicfBoot {
 	
 	public static void main(String[] args) {
@@ -56,48 +57,47 @@ public class SicfBoot {
 		final CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
 		config.setAllowedOrigins(Collections.singletonList("*"));
-		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+		config.setAllowedHeaders(
+				Arrays.asList("Access-Control-Allow-Origin", "Content-Type", "Accept", JWTUtil.JWT_ENCABEZADO));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
 	}
 	
-    /**
-     * Permite establecer la lista de {@link ViewResolver} que serán utilizados.
-     * 
-     * @param manager
-     *            negociador que determina el {@code media type} solicitado en
-     *            una petición
-     * @return {@link ContentNegotiatingViewResolver}
-     */
-    @Bean
-    public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager)
-    {
-        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
-        resolver.setContentNegotiationManager(manager);
-        List<ViewResolver> resolvers = new ArrayList<>();
-        resolvers.add(resourceBundleViewResolver());
-        resolver.setViewResolvers(resolvers);
-        return resolver;
-    }
-
+	/**
+	 * Permite establecer la lista de {@link ViewResolver} que serán utilizados.
+	 *
+	 * @param manager negociador que determina el {@code media type} solicitado en
+	 *                una petición
+	 * @return {@link ContentNegotiatingViewResolver}
+	 */
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setContentNegotiationManager(manager);
+		List<ViewResolver> resolvers = new ArrayList<>();
+		resolvers.add(resourceBundleViewResolver());
+		resolver.setViewResolvers(resolvers);
+		return resolver;
+	}
+	
 	/**
 	 * Define un {@link ResourceBundleViewResolver} que permite el procesamiento
 	 * de los templates definidos en el archivo <b>views/views.properties</b>.
-	 * 
+	 *
 	 * @return {@link ResourceBundleViewResolver}
 	 */
 	@Bean
-	public ResourceBundleViewResolver resourceBundleViewResolver()
-	{
+	public ResourceBundleViewResolver resourceBundleViewResolver() {
 		ResourceBundleViewResolver resolver = new ResourceBundleViewResolver();
 		resolver.setOrder(0);
 		resolver.setBasename("views/views");
 		return resolver;
 	}
-
+	
 	/**
 	 * Configuracion para carga de archivos
+	 *
 	 * @return {@link CommonsMultipartResolver }
 	 */
 	@Bean(name = "multipartResolver")
