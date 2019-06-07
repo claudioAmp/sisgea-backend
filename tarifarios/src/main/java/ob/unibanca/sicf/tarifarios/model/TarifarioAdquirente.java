3d6de37a62545db665d70445cd5d65a23d230b17
carlaulloa <carla.ulloa.lp@gmail.com>
2019-06-06 21:57:33 -0500

    fix: tarifario adquirente

--- tarifarios/src/main/java/ob/unibanca/sicf/tarifarios/model/TarifarioAdquirente.java
+++ tarifarios/src/main/java/ob/unibanca/sicf/tarifarios/model/TarifarioAdquirente.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ob.commons.validation.validation.IdNumerico;
+import ob.commons.validation.validation.NumeroNoRequerido;
 import ob.commons.validation.validation.group.IRegistro;
 
 @Data
@@ -26,10 +27,11 @@ public class TarifarioAdquirente {
 	private int rangoInicial;
 	@IdNumerico(maxRange = 99999999, groups = IRegistro.class)
 	private int rangoFinal;
+	@NumeroNoRequerido(minRange = 0, maxRange = 999)
 	private int idMoneda;
 	private String descripcionMoneda;
-	private Double tarifaFlat;
-	private Double tarifaPorcentual;
-	private int aplicaIGV;
+	private double tarifaFlat;
+	private double tarifaPorcentual;
+	private boolean aplicaIGV;
 
 }
