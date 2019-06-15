4d74c2e96ad9152f351d00ac8950b7534f61df29
Karen MZ <karen.munoz@unmsm.edu.pe>
2019-06-14 22:02:37 -0500

    fix:  indexLastOfRows Página

--- consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/model/criterio/paginacion/Pagina.java
+++ consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/model/criterio/paginacion/Pagina.java
@@ -33,23 +33,22 @@ public class Pagina<S,T>   implements Serializable {
 
     private boolean isLastPage = false;
    
-    private Integer indexFirstOfRows;
+    private int indexFirstOfRows;
    
-    private Integer indexLastOfRows;
+    private int indexLastOfRows;
     
     public Pagina(S criterioBusqueda ,List<T> list) {
         if (list instanceof Page) {
             Page page = (Page) list;
             this.pageNum = page.getPageNum();
             this.pageSize = page.getPageSize();
-
             this.pages = page.getPages();
             this.listaDatos = page;
             this.total = page.getTotal();
-            
-            this.indexFirstOfRows= pageSize*(pageNum-1) +1;
-            this.indexLastOfRows= pageSize*(pageNum-1) ;
             this.criterioBusqueda=criterioBusqueda;
+            this.indexFirstOfRows= page.getStartRow();
+            this.indexLastOfRows= page.getEndRow();
+                      
             }
       }
        
