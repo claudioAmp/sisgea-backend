9aaec4e39f4cbe99669af8196cc1726d56c04fc2
jackore <39741286+jackore@users.noreply.github.com>
2019-07-07 01:56:22 -0500

    feat: adición fragmentoFiltrosOrdenaciones LogContable

--- sicf-consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/model/criterio/compensacion/CriterioBusquedaCompensacion.java
+++ sicf-consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/model/criterio/compensacion/CriterioBusquedaCompensacion.java
@@ -30,4 +30,58 @@ public class CriterioBusquedaCompensacion {
 	List<Integer> institucionesReceptoras;
 	List<String> bines;
 	List<String> codigosRespuesta;
+	
+	//filtros
+	
+	private String filtroFechaProceso;
+	private String filtroIdSecuencia;
+	private String filtroIdMembresia;
+	private String filtroIdServicio;
+	private String filtroIdOrigen;
+	private String filtroIdClaseTransaccion;
+	private String filtroIdCodigoTransaccion;
+	private String filtroIdRol;
+	private String filtroIdCanal;
+	private String filtroIdInstitucionEmisora;
+	private String filtroIdInstitucionReceptora;
+	private String filtroNumeroTarjeta;
+	private String filtroBin;
+	private String filtroIdMonedaCompensacion;
+	private String filtroValorCompensacion;
+	private String filtroFechaSwitch;
+	private String filtroHoraTransaccion;
+	private String filtroCodigoRespuesta;
+	private String filtroNumeroTrace;
+	private String filtroCodigoAutorizacion;
+	private String filtroReferenciaIntercambio;
+	private String filtroGiroNegocio;
+	private String filtroNombreAdquirente;
+	private String filtroCiudadAdquirente;
+	private String filtroPaisAdquirente;
+	
+	private String ordenFechaProceso;
+	private String ordenIdSecuencia;
+	private String ordenIdMembresia;
+	private String ordenIdServicio;
+	private String ordenIdOrigen;
+	private String ordenIdClaseTransaccion;
+	private String ordenIdCodigoTransaccion;
+	private String ordenIdRol;
+	private String ordenIdCanal;
+	private String ordenIdInstitucionEmisora;
+	private String ordenIdInstitucionReceptora;
+	private String ordenNumeroTarjeta;
+	private String ordenBin;
+	private String ordenIdMonedaCompensacion;
+	private String ordenValorCompensacion;
+	private String ordenFechaSwitch;
+	private String ordenHoraTransaccion;
+	private String ordenCodigoRespuesta;
+	private String ordenNumeroTrace;
+	private String ordenCodigoAutorizacion;
+	private String ordenReferenciaIntercambio;
+	private String ordenGiroNegocio;
+	private String ordenNombreAdquirente;
+	private String ordenCiudadAdquirente;
+	private String ordenPaisAdquirente;
 }
