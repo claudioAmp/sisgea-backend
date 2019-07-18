47a0591cd4f6cc5905d1c36e0ca4999d9dc3360c
Mario Cortez Escarate <mario.cortez1@unmsm.edu.pe>
2019-07-18 00:36:50 -0500

    fix: tipo dato operador-tipo-dato

--- sicf-generador-consultas/src/main/java/ob/unibanca/sicf/generadorconsultas/model/OperadorTipoDato.java
+++ sicf-generador-consultas/src/main/java/ob/unibanca/sicf/generadorconsultas/model/OperadorTipoDato.java
@@ -10,11 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OperadorTipoDato {
-	private int idTipoDato;
-    private String tipoDato;
 	private int idOperador;
+	private String idTipoDato;
+	private int idTipoOperador;
+    private String tipoDato;
 	private String simboloOperador;
 	private String operador;
-	private int idTipoOperador;
 	private String tipoOperador;
 }
