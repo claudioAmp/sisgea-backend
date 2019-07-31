3ea41d6ef6af89a81421b046244481b0396a776e
hanzllcc <hanzllcc@gmail.com>
2019-07-31 04:02:53 -0500

    chore: Eliminación del flag "canalSeguro" chore: Externalización de los archivos de configuración usando la variable del sistema "UBA_SICF_CFG". Solo aplica a despliegues como WAR

--- sicf-boot/src/main/java/ob/unibanca/sicf/boot/SicfServletInitializer.java
+++ sicf-boot/src/main/java/ob/unibanca/sicf/boot/SicfServletInitializer.java
@@ -1,12 +1,44 @@
 package ob.unibanca.sicf.boot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.io.File;
+
 public class SicfServletInitializer extends SpringBootServletInitializer {
+	
+	private static final String UBA_SICF_CFG = "UBA_SICF_CFG";
+	private static final String ARCHIVO_APP_PROPERTIES = "application.properties";
+	private static Logger log = LoggerFactory.getLogger(SicfServletInitializer.class);
 	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SicfBoot.class);
+		String rutaArchivoConfigSICF = System.getenv(UBA_SICF_CFG);
+		if (rutaArchivoConfigSICF != null) {
+			log.info("La variable de entorno {} tiene el valor de {}", UBA_SICF_CFG, rutaArchivoConfigSICF);
+			if (!rutaArchivoConfigSICF.endsWith(File.separator)) {
+				log.info("Añadiendo separador: {}", File.separator);
+				rutaArchivoConfigSICF = rutaArchivoConfigSICF.concat(File.separator);
+			}
+			String rutaAppProperties = rutaArchivoConfigSICF.concat(ARCHIVO_APP_PROPERTIES);
+			log.info("Verificando existencia del archivo de configuración {} definido en la variable de entorno {}",
+					rutaAppProperties, UBA_SICF_CFG);
+			if (new File(rutaAppProperties).exists()) {
+				log.info("El archivo de configuración {} existe", rutaAppProperties);
+				log.info("Configurando spring.config.location: {}", rutaArchivoConfigSICF);
+				return application.sources(SicfBoot.class).properties(
+						"spring.config.location:".concat(rutaArchivoConfigSICF));
+			} else {
+				log.info("El archivo de configuración {} no existe", rutaAppProperties);
+				log.info("Cargando archivos de configuración del empaquetado WAR");
+				return application.sources(SicfBoot.class).properties();
+			}
+		} else {
+			log.info("La variabla de entorno {} no existe", UBA_SICF_CFG);
+			log.info("Cargando archivos locales de configuración del empaquetado WAR");
+			return application.sources(SicfBoot.class).properties();
+		}
 	}
 }
