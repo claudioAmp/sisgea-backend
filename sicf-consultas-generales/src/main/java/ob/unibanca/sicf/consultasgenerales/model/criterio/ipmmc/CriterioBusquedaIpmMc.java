56167d362a8b35b8c065045aa164bbfbd483d3be
JhonathanZevallos <wzevallosq@odybank.com.pe>
2019-08-01 20:46:18 -0500

    Cambios Criterio Busqueda IPM

--- sicf-consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/model/criterio/ipmmc/CriterioBusquedaIpmMc.java
+++ sicf-consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/model/criterio/ipmmc/CriterioBusquedaIpmMc.java
@@ -37,7 +37,8 @@ public class CriterioBusquedaIpmMc {
 	
 	//Filtro para Llamar Detalle del IPM
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
-	Date dateLocalTxn;
+	//Date dateLocalTxn;
+	Date fechaProceso;
 	String tramsKey;
 	
 	//filtros Grilla
