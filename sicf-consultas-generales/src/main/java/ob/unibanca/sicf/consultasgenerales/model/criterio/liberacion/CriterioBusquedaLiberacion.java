d3c4573730714c93f0d4d78b2f1f6089341e7ec9
jackore <39741286+jackore@users.noreply.github.com>
2019-11-26 21:06:16 -0500

    feat: Adición .xml consulta liberadas

--- sicf-consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/model/criterio/liberacion/CriterioBusquedaLiberacion.java
+++ sicf-consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/model/criterio/liberacion/CriterioBusquedaLiberacion.java
@@ -17,18 +17,83 @@ import lombok.NoArgsConstructor;
 public class CriterioBusquedaLiberacion {
 	
 	String estadoTransaccion;
-	List<String> origenesArchivo;
-	Integer indConciliacion;
-	Integer transactionCurrency;
-	List<String> messagesTypeInd;
+	String accountNumber;
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	Date fechaTransaccionInicio; // transmissionDate
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	Date fechaTransaccionFin; // transmissionDate
-	String accountNumber;
-	String traceNumber;
-	Long secuenciaSwdmplog;
+	List<Integer> idsOrigen;
+	List<Integer> institucionesEmisoras;
+	List<Integer> institucionesReceptoras;
+	List<String> origenesArchivo;
+	Integer indConciliacion;
+	Integer transactionCurrency;
+	
+	//filtros
+	private String filtroEstadoTransaccion;
+	private String filtroMessageType;
+	private String filtroAccountNumber;
+	private String filtroTransactionCurrency;
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
-	Date fechaTransaccion;
+	private Date filtroTransmissionDate;
+	private String filtroTransmissionTime;
+	private String filtroTraceNumber;
+	private String filtroPosConditionCode;
+	private String filtroResponseCode;
+	private String filtroOrigenArchivo;
+	private String filtroIndConciliacion;
+	private String filtroProcessingCode;
+	private String filtroReferenceNumber;
+	private String filtroAccountIdentification1;
+	private String filtroAccountIdentification2;
+	private String filtroAuthorizationCode;
+	private String filtroIdInstitucionEmisora;
+	private String filtroIdInstitucionReceptora;
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private Date filtroCaptureDate;
+	private String filtroIdMembresia;
+	private String filtroIdOrigen;
+	private String filtroIdProceso;
+	private String filtroIdProducto;
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private Date filtroFechaRegistro;
+	private String filtroHoraRegistro;
+	private String filtroSecuenciaSwdmplog;
+	
+	//Orden
+	private String ordenEstadoTransaccion;
+	private String ordenMessageType;
+	private String ordenAccountNumber;
+	private String ordenTransactionAmount;
+	private String ordenTransactionCurrency;
+	private String ordenSettlementAmount;
+	private String ordenTransmissionDate;
+	private String ordenTransmissionTime;
+	private String ordenTraceNumber;
+	private String ordenPosConditionCode;
+	private String ordenResponseCode;
+	private String ordenOrigenArchivo;
+	private String ordenIndConciliacion;
+	private String ordenProcessingCode;
+	private String ordenReferenceNumber;
+	private String ordenAccountIdentification1;
+	private String ordenAccountIdentification2;
+	private String ordenAuthorizationCode;
+	private String ordenIdInstitucionEmisora;
+	private String ordenIdInstitucionReceptora;
+	private String ordenCaptureDate;
+	private String ordenIdMembresia;
+	private String ordenIdOrigen;
+	private String ordenIdProceso;
+	private String ordenIdProducto;
+	private String ordenFechaRegistro;
+	private String ordenHoraRegistro;
+	private String ordenSecuenciaSwdmplog;
+	
+	//Criterios de filtros de encabezado aggrid tipo RANGO
+	private Double rangoTransactionAmountMin;
+	private Double rangoTransactionAmountMax;
+	private Double rangoSettlementAmountMin;
+	private Double rangoSettlementAmountMax;
 	
 }
