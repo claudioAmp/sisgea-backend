0c68ff134dad323a82ce06afda1c00b27594ed38
Dan <jhairguz.t@gmail.com>
2019-12-05 15:21:12 -0500

    feat: institucion modificada para más campos

--- sicf-mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/model/Institucion.java
+++ sicf-mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/model/Institucion.java
@@ -17,25 +17,58 @@ public class Institucion {
 	
 	@IdNumerico(maxRange = 99999, groups = IRegistro.class)
 	private Integer idInstitucion;
+
 	@CaracterRequerido(maxLength = 80)
 	private String descripcionInstitucion;
+
 	@CaracterRequerido(maxLength = 20)
 	private String descripcionCorta;
+
 	@CaracterNoRequerido(minLength = 1, maxLength = 40)
 	private String razonSocial;
+
 	@CaracterNoRequerido(minLength = 11, maxLength = 12, regexpPattern = "[0-9]+")
 	private String ruc;
+
 	@CaracterNoRequerido(minLength = 1, maxLength = 120)
 	private String direccion;
+
 	private boolean esBancoAdministrador;
+
 	private String tipoInstitucion;
 	private String descripcionTipoInstitucion;
+
 	private boolean esProcesador;
+
 	@CaracterNoRequerido(minLength = 1, maxLength = 15)
 	private String cuentaContableMonedaBase;
+
 	@CaracterNoRequerido(minLength = 1, maxLength = 15)
 	private String cuentaContableMonedaAlterna;
+
 	@IdNumerico(maxRange = 99999)
 	private Integer idInstitucionOperador;
 	private String descripcionInstitucionOpe;
+
+	// nuevos datos
+	private Integer tipoAdquirente;
+	private Boolean cuentaCompensacion;
+	private Boolean esAdquirenteAtm;
+	private Boolean esAdquirenteVentanilla;
+	private Boolean activo;
+    private Boolean esMiembroPrincipalVisa;
+    private Boolean esMiembroPrincipalMc;
+    private String bidVisa;
+    private String icaMcEmisor;
+    private String icaMcAdquirente;
+    private String pcrCreditoVisa;
+	private String pcrDebitoVisa;
+	private Boolean esLogAntiguoContable;
+	private Boolean siAplicaBancoAdministrador;
+	private Integer idInstitucionBidNoPropio;
+	private String descripcionInstitucionBidNoPropio;
+	private Boolean siCompensaOpeSoles;
+	private Boolean siCompensaOpeDolares;
+	private Boolean siAplicaBidVisaPropio;
+	private Boolean siCompensaOpeBidVisa;
 }
