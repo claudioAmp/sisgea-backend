986ace70d4446c5e6f34be75c93c0f3b8e7b454d
Karen <45034018+karenmunozzavaleta@users.noreply.github.com>
2019-12-06 14:44:01 -0500

    feat: service irregular

---
+++ sicf-consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/service/irregular/IIrregularService.java
@@ -0,0 +1,15 @@
+package ob.unibanca.sicf.consultasgenerales.service.irregular;
+
+
+import com.github.pagehelper.Page;
+import ob.unibanca.sicf.consultasgenerales.model.criterio.irregular.CriterioBusquedaIrregular;
+import ob.unibanca.sicf.consultasgenerales.model.irregular.Irregular;
+
+
+
+public interface IIrregularService {
+
+	public Page<Irregular> buscarTodosIrregulares();
+
+	public Page<Irregular> buscarPorCriteriosIrregulares(CriterioBusquedaIrregular criterio , int pageNo, int pageSize);
+}
