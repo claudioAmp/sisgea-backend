6808b6f2b3b8a3f32301f295689da5d5a423274c
Dan <jhairguz.t@gmail.com>
2019-12-08 09:58:05 -0500

    feat: BinRestController para busqueda x membresia & producto impl.

--- sicf-mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/controller/rest/ProgramaBINRestController.java
+++ sicf-mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/controller/rest/ProgramaBINRestController.java
@@ -32,20 +32,20 @@ public class ProgramaBINRestController {
     }
 
     @PreAuthorize("hasPermission('MANT_PROGRAMA_BIN', '2')")
+    @GetMapping(value ="/{id-membresia}/{id-producto}")
+    public List<ProgramaBIN> buscarProgramasBinPorMembresiaYProducto(
+            @IdCadena(maxLength = 1, regexpPattern = "[a-zA-Z]") @PathVariable("id-membresia") String idMembresia,
+            @IdNumerico(maxRange = 99999) @PathVariable("id-producto") Integer idProducto) {
+        return this.programaBINService.buscarProgramasBinPorMembresiaYProducto(idMembresia, idProducto);
+    }
+
+    @PreAuthorize("hasPermission('MANT_PROGRAMA_BIN', '2')")
     @GetMapping(value ="/{id-membresia}/{id-producto}/{id-programa}")
     public ProgramaBIN buscarProgramaBin(
         @IdCadena(maxLength = 1, regexpPattern = "[a-zA-Z]") @PathVariable("id-membresia") String idMembresia,
         @IdNumerico(maxRange = 99999) @PathVariable("id-producto") Integer idProducto,
         @IdNumerico(maxRange = 99999) @PathVariable("id-programa") Integer idPrograma) {
         return this.programaBINService.buscarProgramaBin(idMembresia, idProducto, idPrograma);
-    }
-
-    @PreAuthorize("hasPermission('MANT_PROGRAMA_BIN', '2')")
-    @GetMapping(value ="/{id-membresia}/{id-producto}")
-    public List<ProgramaBIN> buscarProgramasBinPorMembresiaYProducto(
-            @IdCadena(maxLength = 1, regexpPattern = "[a-zA-Z]") @PathVariable("id-membresia") String idMembresia,
-            @IdNumerico(maxRange = 99999) @PathVariable("id-producto") Integer idProducto) {
-        return this.programaBINService.buscarProgramasBinPorMembresiaYProducto(idMembresia, idProducto);
     }
 
     @PreAuthorize("hasPermission('MANT_PROGRAMA_BIN', '1')")
