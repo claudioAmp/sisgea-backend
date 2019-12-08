8a0b9396b8f8772134090aa417318f34bd6886aa
Dan <jhairguz.t@gmail.com>
2019-12-07 21:14:00 -0500

    feat: model BIN modificado, atributos agregados

--- sicf-mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/model/BIN.java
+++ sicf-mantenimientos-generales/src/main/java/ob/unibanca/sicf/mantenimientosgenerales/model/BIN.java
@@ -25,20 +25,42 @@ public class BIN {
 	private double valorOIF;
 	@NumeroNoRequerido(maxRange = 99)
 	private Integer longitudPAN;
+
 	@IdNumerico(maxRange = 99999)
 	private Integer idInstitucion;
 	private String descripcionInstitucion;
 	private String descripcionCortaInstitucion;
+
 	@IdCadena(minLength = 1, maxLength = 1, regexpPattern = "[a-zA-Z]")
 	private String idMembresia;
 	private String descripcionMembresia;
+
 	@IdCadena(minLength = 1, maxLength = 1, regexpPattern = "[a-zA-Z]")
 	private String idServicio;
 	private String descripcionServicio;
+
 	@IdNumerico(minRange = 0, maxRange = 999)
 	private Integer idMonedaBase;
 	private String descripcionMonedaBase;
+
 	@IdNumerico(minRange = 0, maxRange = 999)
 	private Integer idMonedaAlterna;
 	private String descripcionMonedaAlterna;
+
+	// TODO(): idProducto es obligatorio?
+	private Integer idProducto;
+	private String descripcionProducto;
+
+	// TODO(): idPrograma es obligatorio?
+	private Integer idPrograma;
+	private String abreviaturaPrograma;
+
+	private Boolean sreVisaSoles;
+	private Boolean sreVisaDolares;
+	private Boolean siCompensaOpeBinVisa;
+	private Boolean siCompensaOpeSreSolesVisa;
+	private Boolean siCompensaOpeSreDolaresVisa;
+	private Boolean siFacturaEventoBinVisa;
+	private Boolean siFacturaEventoSreSolesVisa;
+	private Boolean siFacturaEventoSreDolaresVisa;
 }
