440f7a3ad17b1f3e25fda66c8528829cb3b6115b
Mario Cortez Escarate <mario.cortez1@unmsm.edu.pe>
2019-12-11 22:51:59 -0500

    feat: rango bines visa, criterios multiples

--- sicf-facturacion/src/main/java/ob/unibanca/sicf/facturacion/service/cargacobrosmiscelaneos/CargaCobrosMiscelaneosVisaService.java
+++ sicf-facturacion/src/main/java/ob/unibanca/sicf/facturacion/service/cargacobrosmiscelaneos/CargaCobrosMiscelaneosVisaService.java
@@ -5,7 +5,10 @@ import ob.commons.error.model.ErrorResponse;
 import ob.commons.excel.CargaArchivoExcel;
 import ob.unibanca.sicf.facturacion.mapper.ICargaCobrosMiscelaneosMapper;
 import ob.unibanca.sicf.facturacion.mapper.IProcesoProcedureMapper;
+import ob.unibanca.sicf.facturacion.model.FacturacionVisa;
 import ob.unibanca.sicf.facturacion.model.MetadataProcedure;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -15,7 +18,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -44,7 +49,9 @@ public class CargaCobrosMiscelaneosVisaService implements ICargaCobrosMiscelaneo
 	@Transactional(propagation = Propagation.REQUIRED)
 	public void cargarRegistro(Map<String, Object> registro) {
 		Map<String, Object> general = new HashMap<>();
-		
+		System.out.println(procedure);
+		System.out.println(registro);
+		System.out.println(general);
 		// Cargando registro
 		general.put("procedureName", procedure.getNombreProcedure());
 		general.put("salida", "");
@@ -59,19 +66,54 @@ public class CargaCobrosMiscelaneosVisaService implements ICargaCobrosMiscelaneo
 		for (MultipartFile multipartfile : multipartfiles) {
 			List<ErrorResponse> aux;
 			String filename = multipartfile.getOriginalFilename();
+			String extension = filename.substring(filename.lastIndexOf(".") + 1);
 			boolean esReproceso = procesoProcedureMapper.isReproceso(filename);
 			HashMap<String, Object> parametrosAdicionales = new HashMap<>();
 			parametrosAdicionales.put("filename", filename);
 			parametrosAdicionales.put("reproceso", esReproceso);
 			
+			
 			try (BufferedInputStream bis = new BufferedInputStream(multipartfile.getInputStream())) {
-				aux = CargaArchivoExcel.readExcelFile(filename, bis, this.procedure.getParametrosProcedure(), this,
-				                                      true, 0, this.procedure.getPatronFechaArchivo(), esReproceso,
-				                                      parametrosAdicionales);
+				List<Map<String, Object>> listaCargada = CargaArchivoExcel.readExcelFile(filename, 
+													  bis, 
+													  this.procedure.getParametrosProcedure(), 
+													  this,
+				                                      true, 
+				                                      0, 
+				                                      this.procedure.getPatronFechaArchivo(), 
+				                                      esReproceso,
+				                                      parametrosAdicionales,
+				                                      //Nuevo
+				                                      extension);
+				System.out.println(listaCargada);
+				listaCargada.forEach(registroCargado ->{
+					@SuppressWarnings("unchecked")
+					Map<String, Object> params = (Map<String, Object>) registroCargado;
+					FacturacionVisa facturacionVisa = FacturacionVisa.builder()
+							.billingPeriod(params.get("BILLING_PERIOD").toString())
+							//.invoiceDate(new Date(params.get("INVOICE_DATE").toString()))
+							.name(params.get("NAME").toString())
+							.invoiceId(params.get("INVOICE_ID").toString())
+							.subInvoice(params.get("SUB_INVOICE").toString())
+							.entityType(params.get("ENTITY_TYPE").toString())
+							.entityId(params.get("ENTITY_ID").toString())
+							.binMap(params.get("BIN_MAP").toString())
+							.settlementId(params.get("SETTLEMENT_ID").toString())
+							.description(params.get("DESCRIPTION").toString())
+							.billingLine(params.get("BILLING_LINE").toString())
+							.type(params.get("TYPE").toString())
+							.rateType(params.get("RATE_TYPE").toString())
+							.units(Integer.parseInt(params.get("UNITS").toString()))
+							.rateCur(params.get("RATE").toString())
+							.rate(Double.parseDouble(params.get("RATE").toString()))
+							.foreingExchangeRate(Integer.parseInt(params.get("FOREIGN_EXCHANGE_RATE").toString()))
+							.billingCurrency(params.get("BILLING_CURRENCY").toString())
+							.total(Double.parseDouble(params.get("TOTAL").toString()))
+							.build();
+					System.out.println(facturacionVisa);
+				});
 				
-				if (aux != null) {
-					aux.stream().filter(Objects::nonNull).forEachOrdered(listaExcepciones::add);
-				}
+			
 			} catch (IOException e) {
 				throw new RecursoNoEncontradoException(e.getMessage());
 			}
