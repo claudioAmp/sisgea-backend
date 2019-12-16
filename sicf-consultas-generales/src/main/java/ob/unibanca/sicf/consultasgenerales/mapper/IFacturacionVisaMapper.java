78d6abf81fa7bd2b0e23022d8fa625d6b1a4aa5d
Karen <45034018+karenmunozzavaleta@users.noreply.github.com>
2019-12-15 20:03:56 -0500

    feat: factura Visa

---
+++ sicf-consultas-generales/src/main/java/ob/unibanca/sicf/consultasgenerales/mapper/IFacturacionVisaMapper.java
@@ -0,0 +1,13 @@
+package ob.unibanca.sicf.consultasgenerales.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import com.github.pagehelper.Page;
+import ob.unibanca.sicf.consultasgenerales.model.criterio.facturavisa.CriterioBusquedaFacturaVisa;
+import ob.unibanca.sicf.consultasgenerales.model.facturavisa.FacturaVisa;
+
+@Mapper
+public interface IFacturacionVisaMapper {
+	
+	public Page<FacturaVisa> buscarPorCriterios(CriterioBusquedaFacturaVisa criterioPaginacion);
+
+}
