508fbe94ad5143bad5425ba1ba86aabd8bdd678b
claudioAmp <claudioandree40@gmail.com>
2020-02-10 13:21:41 -0500

    feat: adición de rest controller a horario y detalle horario

--- sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/controller/rest/DetalleHorarioRestController.java
+++ sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/controller/rest/DetalleHorarioRestController.java
@@ -1,4 +1,31 @@
 package edu.taller.sisgea.mantenimientosgenerales.controller.rest;
 
+import edu.taller.sisgea.mantenimientosgenerales.model.DetalleHorario;
+import edu.taller.sisgea.mantenimientosgenerales.model.resultadocarga.ResultadoCarga;
+import edu.taller.sisgea.mantenimientosgenerales.service.detallerhorario.IDetalleHorarioService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public class DetalleHorarioRestController {
+
+    private final IDetalleHorarioService detalleHorarioService;
+
+    public DetalleHorarioRestController(IDetalleHorarioService detalleHorarioService) {
+        this.detalleHorarioService = detalleHorarioService;
+    }
+
+    @GetMapping(value = "/detalleHorario")
+    public List<DetalleHorario> buscarTodosDetalleHorario() {
+        return this.detalleHorarioService.buscarTodosDetalleHorario();
+    }
+
+    @PostMapping(value = "/detalleHorario/cargar")
+    public List<ResultadoCarga> cargarArchivo(@RequestParam("file[]") List<MultipartFile> file){
+        return this.detalleHorarioService.cargarArchivos(file);
+    }
+
 }
--- sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/controller/rest/HorarioRestController.java
+++ sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/controller/rest/HorarioRestController.java
@@ -1,7 +1,31 @@
 package edu.taller.sisgea.mantenimientosgenerales.controller.rest;
 
+import edu.taller.sisgea.mantenimientosgenerales.model.Horario;
+import edu.taller.sisgea.mantenimientosgenerales.model.resultadocarga.ResultadoCarga;
+import edu.taller.sisgea.mantenimientosgenerales.service.horario.IHorarioService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public class HorarioRestController {
 
+    private final IHorarioService horarioService;
 
+    public HorarioRestController(IHorarioService horarioService) {
+        this.horarioService = horarioService;
+    }
+
+    @GetMapping(value = "/horario")
+    public List<Horario> buscarTodosDetalleHorario() {
+        return this.horarioService.buscarTodosHorario();
+    }
+
+    @PostMapping(value = "/horario/cargar")
+    public List<ResultadoCarga> cargarArchivo(@RequestParam("file[]") List<MultipartFile> file){
+        return this.horarioService.cargarArchivos(file);
+    }
 
 }
