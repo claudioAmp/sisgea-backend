a9d0f5bf01de96e863bd040c2c007b6cce52cb14
claudioAmp <claudioandree40@gmail.com>
2020-02-10 17:06:43 -0500

    feat: beack de llamada a proceso de asignacion de espacio académico

--- sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/mapper/IEspacioAcademicoMapper.java
+++ sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/mapper/IEspacioAcademicoMapper.java
@@ -1,14 +1,21 @@
 package edu.taller.sisgea.mantenimientosgenerales.mapper;
 
+import edu.taller.sisgea.mantenimientosgenerales.model.resultadoasignacionespaciosacademicos.ResultadoAsignacionEspaciosAcademicos;
 import ob.commons.mantenimiento.mapper.IMantenibleMapper;
 import edu.taller.sisgea.mantenimientosgenerales.model.EspacioAcademico;
 
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
 public interface IEspacioAcademicoMapper extends IMantenibleMapper<EspacioAcademico> {
 	
 	Optional<EspacioAcademico> buscarEspacioAcademico(Integer idEspacioAcademico);
+
+	public void ejecutarProcedimientoAsignacionEspacioAcademico();
+
+	public List<ResultadoAsignacionEspaciosAcademicos> buscarTodosResultados ();
 	
 }
