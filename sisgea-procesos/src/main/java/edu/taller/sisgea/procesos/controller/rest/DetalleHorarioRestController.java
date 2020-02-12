a470d83013613bd113fa3e2cc795c3ba9b41c608
CristhianAlvarez1 <47466640+CristhianAlvarez1@users.noreply.github.com>
2020-02-12 17:37:51 -0500

    fix: @RestController en DetalleHorario

--- sisgea-procesos/src/main/java/edu/taller/sisgea/procesos/controller/rest/DetalleHorarioRestController.java
+++ sisgea-procesos/src/main/java/edu/taller/sisgea/procesos/controller/rest/DetalleHorarioRestController.java
@@ -3,13 +3,18 @@ package edu.taller.sisgea.procesos.controller.rest;
 import edu.taller.sisgea.procesos.model.DetalleHorario;
 import edu.taller.sisgea.procesos.model.resultadocarga.ResultadoCarga;
 import edu.taller.sisgea.procesos.service.detallehorario.IDetalleHorarioService;
+
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@Validated
+@RestController
 public class DetalleHorarioRestController {
 
     private final IDetalleHorarioService detalleHorarioService;
