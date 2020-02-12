22ea6a0d9d802d5cbcba05de96eada8c1cc740ff
jackore <39741286+jackore@users.noreply.github.com>
2020-02-12 10:13:31 -0500

    fix: limpieza mantenimientos- consultas generales

--- sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/mapper/IParametrosGeneralesMapper.java
+++ sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/mapper/IParametrosGeneralesMapper.java
@@ -4,8 +4,6 @@ import edu.taller.sisgea.mantenimientosgenerales.model.ParametrosGenerales;
 import ob.commons.mantenimiento.mapper.IMantenibleMapper;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
-
 @Mapper
 public interface IParametrosGeneralesMapper extends IMantenibleMapper<ParametrosGenerales> {
 
--- sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/service/detallerhorario/DetalleHorarioService.java
+++ sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/service/detallerhorario/DetalleHorarioService.java
@@ -27,7 +27,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
