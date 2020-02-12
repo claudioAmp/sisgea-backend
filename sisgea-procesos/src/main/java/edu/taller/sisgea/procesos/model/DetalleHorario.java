ff64cfd400f08b24e467bd7ce693d1ca4526063a
jackore <39741286+jackore@users.noreply.github.com>
2020-02-12 13:48:18 -0500

    feat: Moviendo de mantenimiento a Procesos - Cargas excel

---
+++ sisgea-procesos/src/main/java/edu/taller/sisgea/procesos/model/DetalleHorario.java
@@ -0,0 +1,23 @@
+package edu.taller.sisgea.procesos.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetalleHorario {
+
+    private Integer idHorarioDetalle;
+    private String idCurso;
+    private Integer seccion;
+    private Integer idHorario;
+    private String tipoHorario;
+    private String horarioInicio;
+    private String horarioFin;
+
+}
