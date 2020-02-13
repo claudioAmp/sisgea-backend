2080577a510476b3582b3cae419c5021fc3f944f
Mario Cortez Escarate <mario.cortez1@unmsm.edu.pe>
2020-02-13 12:16:54 -0500

    feat: desc del curso en detalle horario

--- sisgea-procesos/src/main/java/edu/taller/sisgea/procesos/model/DetalleHorario.java
+++ sisgea-procesos/src/main/java/edu/taller/sisgea/procesos/model/DetalleHorario.java
@@ -14,6 +14,7 @@ public class DetalleHorario {
 
     private Integer idHorarioDetalle;
     private String idCurso;
+    private String descCurso;
     private Integer seccion;
     private Integer idHorario;
     private String tipoHorario;
