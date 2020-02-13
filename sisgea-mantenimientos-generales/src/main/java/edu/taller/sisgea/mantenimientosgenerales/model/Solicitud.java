16db8b4ace38f4c080f2a51b34aefd6df535f561
Mario Cortez Escarate <mario.cortez1@unmsm.edu.pe>
2020-02-13 17:53:00 -0500

    feat: ajustes en solcitante

--- sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/model/Solicitud.java
+++ sisgea-mantenimientos-generales/src/main/java/edu/taller/sisgea/mantenimientosgenerales/model/Solicitud.java
@@ -2,6 +2,9 @@ package edu.taller.sisgea.mantenimientosgenerales.model;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,11 +21,16 @@ public class Solicitud {
 	String descripcionEspacioAcademico;
 	Integer idSolicitante;
 	String estadoSolicitud;
+	String dni;
+	String nombres;
+	String apellidoPaterno;
+	String apellidoMaterno;
 	String descripcionEstadoSolicitud;
 	String tipoSolicitud;
 	String descripcionTipoSolicitud;
 	String motivo;
 	Date fechaRegistro;
+	@JsonFormat(pattern="yyyy-MM-dd")
 	Date fechaReserva;
 	String horaInicio;
 	String horaFin;
